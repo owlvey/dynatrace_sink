0f2445969f5dc899aae68b309d617f7e9a96a4a5
Gregory Valderrama <gcvalderrama@hotmail.com>
2021-09-23 21:30:32 -0500

    add

--- run_local_without.ps1
+++ run_local_without.ps1
@@ -1,8 +1,12 @@
-$env:app__start_on="2021-03-25 10:00:00"
-$env:app__dyn_entity="Controller"
-$env:app__dyn_tag=""
-$env:app__dyn_host=""
+$env:app__start_on="2021-06-25 10:00:00"
+$env:app__dyn_entity="Controller" # sufijo 
+$env:app__dyn_tag="unique" # tag de los controladores
+$env:app__dyn_host="https://managed.dynatrace/e/1d4cddc0/api/v1/"
 $env:app__dyn_token=""
 $env:app__dyn_prefix=""
 $env:app__dir_output="./wip"
+$env:app__enable_dyn_metric=1
+$env:app__enable_dyn_metadata=0
+$env:app__enable_dyn_problem=1
 python3 ./startup.py
+
