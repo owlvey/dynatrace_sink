0eaf0a5d27dbe8e500d00dc807dfef962885a621
Gregory Valderrama <gcvalderrama@hotmail.com>
2021-09-23 21:08:34 -0500

    add

---
+++ run_local_without.ps1
@@ -0,0 +1,8 @@
+$env:app__start_on="2021-03-25 10:00:00"
+$env:app__dyn_entity="Controller"
+$env:app__dyn_tag=""
+$env:app__dyn_host=""
+$env:app__dyn_token=""
+$env:app__dyn_prefix=""
+$env:app__dir_output="./wip"
+python3 ./startup.py
